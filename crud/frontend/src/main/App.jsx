c3f739230c30b1cd20f8f62d7d64ce8ee3cb566c
Mario-78 <mariojsws1@gmail.com>
2021-11-24 22:15:39 -0300

    Configuração do router

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -2,16 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import  './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
 
 import Logo from '../components/template/Logo.jsx'
-import Footer from '../components/template/Footer.jsx'
-import Home from '../components/home/Home.jsx'
 import Nav from '../components/template/Nav.jsx'
+import Routes from './Routes.jsx'
+// import Home from '../components/home/Home.jsx'
+// import UserCrud from '../components/user/UserCrud'
+import Footer from '../components/template/Footer.jsx'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            {/* <UserCrud /> */}
+            <Footer />
+        </div>
+    </BrowserRouter>
